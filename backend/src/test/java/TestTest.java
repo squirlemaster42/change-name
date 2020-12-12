80b7887518c514b068985e34b29d7d9c14ea0232
Jakob Misbach <jakobmisbach8@gmail.com>
2020-12-12 16:45:11 -0500

    Added basic backend

---
+++ backend/src/test/java/TestTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestTest {
+
+    @Test
+    public void testTest(){
+        assertTrue(true);
+    }
+}
